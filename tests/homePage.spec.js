08a59d2c4815373703966d36621455fd0a09e526
shahrukh asghar <shanig1188@gmail.com>
2025-05-22 15:25:56 +0500

    Playwright initiate

---
+++ tests/homePage.spec.js
@@ -0,0 +1,17 @@
+const {test,expect}= require('@playwright/test')
+
+
+
+test('Home Page',async ({page})=>{
+await page.goto('https://www.demoblaze.com/')
+
+let pageTitle=  await page.title()
+console.log('This is the page title: ', pageTitle)
+await expect(page).toHaveTitle('STORE')
+
+let pageUrl=  await page.url()
+console.log('This is the page url: ', pageUrl)
+await expect(page).toHaveURL('https://www.demoblaze.com/')
+await page.close()
+
+})
