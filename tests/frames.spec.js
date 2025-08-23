e3fa49e6f720112d3cf96122f0ee4c9b78b006c5
shahrukh asghar <shanig1188@gmail.com>
2025-08-23 19:55:28 +0500

    Iframe added

--- tests/frames.spec.js
+++ tests/frames.spec.js
@@ -8,6 +8,15 @@ test('Handle Frames',async({page})=>{
     console.log('Total number of frames', Allframes.length)
 
     // Approach 1 Using name or url of frame
+    //const frameName= await page.frame('name') // If name is available then we use this code 
+    //const frame1 = await page.frame({url: 'https://ui.vision/demo/webtest/frames/frame_1.html'})
+    //await  frame1.fill('[name="mytext1"]','Hello')
+    //await page.waitForTimeout(2000)
 
-    page.frame({url: ''})
+
+    // Approach 2 using frame locator
+
+    const frameInputBox= await page.frameLocator('frame[src="frame_1.html"]').locator('[name="mytext1"]')
+    frameInputBox.fill('Hello')
+    await page.waitForTimeout(5000)
 })
