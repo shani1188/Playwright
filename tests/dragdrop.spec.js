e70190dcc3b2f9c8d3e11caab84c294b3396f3b6
shahrukh asghar <shanig1188@gmail.com>
2025-08-25 01:12:53 +0500

    drag and drop

--- tests/dragdrop.spec.js
+++ tests/dragdrop.spec.js
@@ -1,1 +1,11 @@
 import {test,expect} from '@playwright/test'
+test('Drag and Drop',async({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    const source = await page.locator('#draggable') //drag me
+    const target = await page.locator('#droppable') // drop here
+    await source.dragTo(target)
+    expect.soft(target).toHaveText('Dropped!')
+    await page.waitForTimeout(5000)
+    
+})
