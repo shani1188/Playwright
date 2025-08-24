b88582442423affd0f26fcfcc630fc3cbed06eae
shahrukh asghar <shanig1188@gmail.com>
2025-08-25 01:28:10 +0500

    Color and Background color assertion added

--- tests/dragdrop.spec.js
+++ tests/dragdrop.spec.js
@@ -5,7 +5,12 @@ test('Drag and Drop',async({page})=>{
     const source = await page.locator('#draggable') //drag me
     const target = await page.locator('#droppable') // drop here
     await source.dragTo(target)
+    const color = await target.evaluate(el=> getComputedStyle(el).color)
+    const bgColor = await target.evaluate(el=> getComputedStyle(el).backgroundColor)
+    console.log('Color: ',color)
     expect.soft(target).toHaveText('Dropped!')
+    expect(color).toBe('rgb(119, 118, 32)')
+    expect(bgColor).toBe('rgb(255, 250, 144)')
     await page.waitForTimeout(5000)
     
 })
